065343e7f1ec579ea128604bd952e06fa0f8ef01
D074024 (jonasschilling) <jonas.schilling@sap.com>
2019-12-12 22:11:35 +0100

    Login-Dropdown entfernt --> sinnlos

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,31 +1,8 @@
 function onLoad() {
-	document.getElementById("typeLogin").value = 0;
-	changeLogin();
 	document.getElementById("type").value = 0;
 	changeFields();
 }
 
-function changeLogin() {
-	console.log("changeLogin at changed value");
-	if (document.getElementById("typeLogin").value == 2) {
-		document.getElementById("namelabelLogin").style.display = '';
-		document.getElementById("namelabelLogin").innerHTML = 'Restaurantname';
-		document.getElementById("usernameLogin").style.display = '';
-		document.getElementById("pwlabel").style.display = '';
-		document.getElementById("pw").style.display = '';
-	} else if (document.getElementById("typeLogin").value == 1) {
-		document.getElementById("namelabelLogin").style.display = '';
-		document.getElementById("namelabelLogin").innerHTML = 'Benutzername';
-		document.getElementById("usernameLogin").style.display = '';
-		document.getElementById("pwlabel").style.display = '';
-		document.getElementById("pw").style.display = '';
-	} else {
-		document.getElementById("namelabelLogin").style.display = 'none';
-		document.getElementById("usernameLogin").style.display = 'none';
-		document.getElementById("pwlabel").style.display = 'none';
-		document.getElementById("pw").style.display = 'none';
-	}
-}
 
 function changeFields() {
 	if (document.getElementById("type").value == 2) {
